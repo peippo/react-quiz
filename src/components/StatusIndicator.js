4e9db06a15f5dd725db418f49ef2b26908aad711
Janne Peippo <peippo@lumous.com>
2018-11-11 15:36:03 +0200

    Adjust status indicator styling

--- src/components/StatusIndicator.js
+++ src/components/StatusIndicator.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const StatusIndicator = ({ answered }) => (
 	<div className="question__status">
-		{answered && <span className="question__saved">&#10003;</span>}
+		{answered && <span className="question__answered"></span>}
 	</div>
 );
 
